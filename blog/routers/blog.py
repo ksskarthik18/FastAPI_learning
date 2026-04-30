dfb7a17691c445af4054927af33db8539d50125a
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-01 03:03:40 +0530

    fastapi learning

---
+++ blog/routers/blog.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter,Depends
+from .. import schemas,models
+from typing import List
+
+
+router = APIRouter()
+
+@router.get('/blog',response_model = List[schemas.ShowBlog],tags=['blogs'])
+def all(db : Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
---
+++ blog/schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import List
+
+class BlogBase(BaseModel):
+    title : str
+    body : str
+    user_id : int
+class Blog(BlogBase):
+    class Config():
+        orm_mode = True
+
+
+class ShowUser(BaseModel):
+    id : int
+    name : str
+    email : str
+    blogs : List[Blog] =[]
+    class Config():
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title : str
+    body : str
+    creator : ShowUser
+    class Config():
+        orm_mode = True
+    
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+
