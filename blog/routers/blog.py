a4c0a1ea8dc6ca616e54958e19cbf73843c7118c
ksskarthik18 <karthik.kirla06@gmail.com>
2026-05-01 03:59:51 +0530

    modified : api route path operators

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,17 +4,20 @@ from typing import List
 from sqlalchemy.orm import Session
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix ='/blog',
+    tags=['Blogs']
+)
 
 get_db = database.get_db
 
-@router.get('/blog',response_model = List[schemas.ShowBlog],tags=['blogs'])
+@router.get('/',response_model = List[schemas.ShowBlog])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@router.post('/blog',status_code =status.HTTP_201_CREATED,tags=['blogs'])
+@router.post('/',status_code =status.HTTP_201_CREATED)
 def create(request : schemas.Blog,db : Session = Depends(get_db) ):
     new_blog = models.Blog(title = request.title,body = request.body,user_id = request.user_id)
     db.add(new_blog)
@@ -23,7 +26,7 @@ def create(request : schemas.Blog,db : Session = Depends(get_db) ):
     return new_blog
 
 
-@router.delete('/blog/{id}',status_code = status.HTTP_204_NO_CONTENT,tags=['blogs'])
+@router.delete('/{id}',status_code = status.HTTP_204_NO_CONTENT)
 def destory(id,db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -34,7 +37,7 @@ def destory(id,db : Session = Depends(get_db)):
     return 'done'
 
 
-@router.put('/blog/{id}',status_code = status.HTTP_202_ACCEPTED,tags=['blogs'])
+@router.put('/{id}',status_code = status.HTTP_202_ACCEPTED)
 def update(id, request : schemas.Blog ,db : Session = Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -46,7 +49,7 @@ def update(id, request : schemas.Blog ,db : Session = Depends(get_db)):
 
 
 
-@router.get('/blog/{id}',status_code=200,response_model = schemas.ShowBlog,tags=['blogs'])
+@router.get('/{id}',status_code=200,response_model = schemas.ShowBlog)
 def show(id,db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
